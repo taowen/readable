09a052ec7e8a58095ac3d7b82465286f9cba71ae
Tao Wen <taowen@gmail.com>
2018-03-23 20:54:22 +0800

    Update intention_revealing_name.java

--- intention_revealing_name.java
+++ intention_revealing_name.java
@@ -5,3 +5,27 @@ int d; // elapsed time in days
 // good
 
 int elapsedTimeInDays;
+
+// bad
+
+public List<int[]> getThem() {   
+  List<int[]> list1 = new ArrayList<int[]>();    
+  for (int[] x : theList) { 
+    if (x[0] == 4) { 
+      list1.add(x); 
+    }
+  }
+  return list1;  
+}
+
+// good
+
+public List<Cell> getFlaggedCells() {    
+  List<Cell> flaggedCells = new ArrayList<Cell>();    
+  for (Cell cell : gameBoard) {
+    if (cell.isFlagged()) {
+      flaggedCells.add(cell); 
+    }
+  }
+  return flaggedCells;  
+}
