747d097c19dda599c10cbfa1d64c1955d1a10912
Tao Wen <taowen@gmail.com>
2018-03-24 07:44:06 +0800

    Update intention_revealing_name.java

--- intention_revealing_name.java
+++ intention_revealing_name.java
@@ -65,3 +65,38 @@ if (user_owns_document) {
 if (!user_owns_document) {
     // document is read-only...
 }
+
+// bad
+
+var update_highlight = function (message_num) {
+ if ($("#vote_value" + message_num).html() === "Up") {
+ $("#thumbs_up" + message_num).addClass("highlighted");
+ $("#thumbs_down" + message_num).removeClass("highlighted");
+ } else if ($("#vote_value" + message_num).html() === "Down") {
+ $("#thumbs_up" + message_num).removeClass("highlighted");
+ $("#thumbs_down" + message_num).addClass("highlighted");
+ } else {
+ $("#thumbs_up" + message_num).removeClass("highighted");
+ $("#thumbs_down" + message_num).removeClass("highlighted");
+ }
+};
+
+// good
+
+var update_highlight = function (message_num) {
+ var thumbs_up = $("#thumbs_up" + message_num);
+ var thumbs_down = $("#thumbs_down" + message_num);
+ var vote_value = $("#vote_value" + message_num).html();
+ var hi = "highlighted";
+ if (vote_value === "Up") {
+ thumbs_up.addClass(hi);
+ thumbs_down.removeClass(hi);
+ } else if (vote_value === "Down") {
+ thumbs_up.removeClass(hi);
+ thumbs_down.addClass(hi);
+ } else {
+ thumbs_up.removeClass(hi);
+ thumbs_down.removeClass(hi);
+ }
+};
+
