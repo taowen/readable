f88e37fd7126fc9275f72e87da28a1e136e838f6
Tao Wen <taowen@gmail.com>
2018-03-24 09:16:15 +0800

    Update explicit_lifecycle.py

--- shorten_process/explicit_lifecycle.py
+++ shorten_process/explicit_lifecycle.py
@@ -3,9 +3,9 @@
 # No use of example_value up to this point.
 if request:
  for value in request.values:
- if value > 0:
- example_value = value
- break
+   if value > 0:
+     example_value = value
+     break
 for logger in debug.loggers:
  logger.log("Example:", example_value)
 
