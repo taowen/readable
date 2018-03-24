3b9a7d667da8c9953d711a4506282e9d7ffa2da2
Tao Wen <taowen@gmail.com>
2018-03-24 08:18:06 +0800

    Create remove_temp_variable.java

---
+++ shorten_process/remove_temp_variable.java
@@ -0,0 +1,25 @@
+// bad
+
+var remove_one = function(array, value_to_remove) {
+    var index_to_remove = null;
+    for (var i = 0; i < array.length; i += 1) {
+        if (array[i] === value_to_remove) {
+            index_to_remove = i;
+            break;
+        }
+    }
+    if (index_to_remove !== null) {
+        array.splice(index_to_remove, 1);
+    }
+};
+
+// good
+
+var remove_one = function(array, value_to_remove) {
+    for (var i = 0; i < array.length; i += 1) {
+        if (array[i] === value_to_remove) {
+            array.splice(i, 1);
+            return;
+        }
+    }
+};
